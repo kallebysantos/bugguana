6e0c29d76a6078fadc0e35757d0b218aa664f697
kallebysantos <kalleby_santos@hotmail.com>
2025-04-17 13:11:10 +0100

    fix: creating extensions in indepodent way

--- supabase/schemas/_deps.sql
+++ supabase/schemas/_deps.sql
@@ -1,9 +1,9 @@
 create schema if not exists private;
 
 -- Embeddings
-create extension vector with schema extensions;
+create extension if not exists vector with schema extensions;
 
 -- Cron
-create extension pg_cron with schema pg_catalog;
+create extension if not exists pg_cron with schema pg_catalog;
 grant usage on schema cron to postgres;
 grant all privileges on all tables in schema cron to postgres;
